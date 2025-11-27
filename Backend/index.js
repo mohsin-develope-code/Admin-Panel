4d92378d1614df9750bae350d1ad1a07174e71f9
Mohammad Mohsin <167190542+mohsin-develope-code@users.noreply.github.com>
2025-11-27 20:01:48 +0530

    Update CORS origin to production URL

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ 
-              origin: 'http://localhost:5173',  
+              origin: 'https://altazkeer-admin-panel.vercel.app',  
               methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
               credentials: true, 
         }));
